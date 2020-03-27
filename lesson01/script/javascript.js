09f2bbc9f47f5cafc7286cede48954caca3c46a9
Vasiliy Emelin <vemelin@gmail.com>
2020-03-27 08:43:25 -0500

    Поправил javascript.js убрал все лишнее и оставил все строго по ТЗ

--- lesson01/script/javascript.js
+++ lesson01/script/javascript.js
@@ -4,26 +4,3 @@ let addExpenses;
 let deposit;
 let mission;
 let period;
-
-// alert ('Доброго дня!');
-
-console.log ('Изучаем git, все идет по плану');
-
-/* function pow(x, n) {
-    var result = 1;
-
-    for (var i=0; i < n; i++) {
-        result *= x;
-    }
-
-    return result;
-}
-
-var x = prompt ('x?', '');
-var n = prompt ('n?', '');
-
-if (n < 0) {
-    alert ('Степень' + n + 'не поддерживается, введите целую степень, большую 0')ж
-} else {
-    alert( pow (x, n) );
-} */
