e9903da02d6839d0afa8623a3552695d60f59763
Vasiliy Emelin <vemelin@gmail.com>
2020-03-26 23:42:15 -0500

    Первый коммит - добавление index.html страницы

---
+++ lesson01/script/javascript.js
@@ -0,0 +1,25 @@
+/* function pow(x, n) {
+    var result = 1;
+
+    for (var i=0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+var x = prompt ('x?', '');
+var n = prompt ('n?', '');
+
+if (n < 0) {
+    alert ('Степень' + n + 'не поддерживается, введите целую степень, большую 0')ж
+} else {
+    alert( pow (x, n) );
+} */
+
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
