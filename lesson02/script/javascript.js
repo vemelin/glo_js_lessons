a0eb91dde4a691923a22d24fbbb34f81f7039309
Vasiliy Emelin <vemelin@gmail.com>
2020-03-28 11:43:35 -0500

    fixed mistake of budgetDay value

--- lesson02/script/javascript.js
+++ lesson02/script/javascript.js
@@ -18,6 +18,5 @@ console.log('Цель заработать ' + mission + ' долларов');
 let period = 24;
 console.log('Период равен ' + period + ' месяцев');
 
-let money = 1000;
 let budgetDay = money/30;
 console.log(budgetDay);
