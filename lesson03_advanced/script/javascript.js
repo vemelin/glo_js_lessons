d440d8a9383855eb6313d4bde9fdd16de92695bf
Vasiliy Emelin <vemelin@gmail.com>
2020-03-30 17:01:30 -0500

    advanced lesson 3 updates

--- lesson03_advanced/script/javascript.js
+++ lesson03_advanced/script/javascript.js
@@ -1,4 +1,11 @@
-let lang = 'ru, en'; 
-(lang[0] == 'ru') ? console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday') :  
-(lang[1] == 'en') ? console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье') :  
+// 1 task
+let lang = ['ru', 'en'];
+(lang[3] == 'ru') ? console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье') :  
+(lang[1] == 'en') ? console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday') :  
 console.log('По вашему запросу ничего не найдено');
+
+// 2 task
+let namePerson = ['Артем', 'Максим'];
+(namePerson[1] == 'Артем') ? console.log('Директор') :  
+(namePerson[3] == 'Максим') ? console.log('Преподаватель') :  
+console.log('Студент');
