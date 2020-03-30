60ea3f08a88a6f572bf309db31b4b77172b31bdd
Vasiliy Emelin <vemelin@gmail.com>
2020-03-30 16:24:25 -0500

    New iteration of updates

--- lesson03/script/javascript.js
+++ lesson03/script/javascript.js
@@ -21,17 +21,17 @@ let period = 24;
 console.log('Период равен ' + period + ' месяцев');
 
 //New criteria from lesson 3
-money = + prompt('Ваш месячный доход?', 5000);
+money = +prompt('Ваш месячный доход?', 5000);
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
 deposit = confirm('Есть ли у вас депозит в банке?');
 
 let expenses1 = prompt('Введите обязательную статью расходов?', 'Expenses 1');
-let amount1 = + prompt('1 Количество: Во сколько это обойдется?', 1500);
+let amount1 = +prompt('1 Количество: Во сколько это обойдется?', 1500);
 
 let expenses2 = prompt('Введите обязательную статью расходов?', 'Expenses 2');
-let amount2 = + prompt('1 Количество: Во сколько это обойдется?', 375);
+let amount2 = +prompt('1 Количество: Во сколько это обойдется?', 375);
 
-let budgetMonth = (money - (amount1 + amount2));
+let budgetMonth = money - (amount1 + amount2);
 console.log(budgetMonth);
 
 let budgetDay = budgetMonth/30;
