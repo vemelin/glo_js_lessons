6cd65f6c88efa678cf21f0cfc92043406976b333
Vasiliy Emelin <vemelin@gmail.com>
2020-03-29 20:58:32 -0500

    JS, brain improvements

--- own/script/javascript.js
+++ own/script/javascript.js
@@ -1,15 +1,38 @@
-let num = 266219;
+let x, y;
+x = 5;
+y = 6;
 
-num = String(num);
-
-let results = 1;
-
-for(let i = 0; i < num.length; i++){
-    results *= num[i];
+// Regular conditions:
+if (y > x) {
+    console.log(true);
+} else if (y < x) {
+    console.log(false);
 }
 
-let res = results ** 3;
-console.log(res);
+// Alternative — short conditions:
+y < x ? console.log(true) : console.log(false);
 
-let message = String(res);
-alert(message.slice(0, 2));
+// Switch condition example
+let res = 5;
+switch (res){
+    case res: console.log(true); break;
+    default: console.log(false);
+}
+
+//Method prompt & confirm
+let person;
+confirm ('Are you sure?') ? person = prompt('Hello, what is your name?') & alert('Hello ' + person) : alert('Person not sure');
+
+/* if(confirm ('Are you sure?')){
+    person = prompt('Hello, what is your name?');
+    alert('Hello ' + person);
+} else {
+    alert('Person not sure');
+} */
+
+
+// Work flow of Array + for statements
+let arr = new Array(1, 2, 'Hello ', 'World', 111.01, 'Value', false, true);
+for (let a = 0; a < arr.length; a++){
+    document.write(arr[a] + '<br/>');
+}
