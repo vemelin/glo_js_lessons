23eb59beca03a460468896c3ce64942e3064d07d
Vasiliy Emelin <vemelin@gmail.com>
2020-03-30 15:23:13 -0500

    first updates

---
+++ lesson03_advanced/script/javascript.js
@@ -0,0 +1,4 @@
+let lang = 'ru, en'; 
+(lang[0] == 'ru') ? console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday') :  
+(lang[1] == 'en') ? console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье') :  
+console.log('По вашему запросу ничего не найдено');
