648aa10d09e0f5dfd9cb316dbb4b5b64d54306e7
Vasiliy Emelin <vemelin@gmail.com>
2020-03-31 12:14:16 -0500

    Advanced lesson03 udpates

--- lesson03_advanced/script/javascript.js
+++ lesson03_advanced/script/javascript.js
@@ -2,9 +2,39 @@
 
 // 1st task
 let lang = prompt('Выберите язык используя короткие префиксы ru или en', '');
-(lang == 'ru') ? console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье') :  
-(lang == 'en') ? console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday') :  
-console.log('По вашему запросу ничего не найдено');
+
+if (lang == 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+} else if (lang == 'en') {
+    console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday')
+} else {
+    console.log('По вашему запросу ничего не найдено');
+}
+
+switch(lang) {
+    case (lang = 'ru'): 
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'); 
+        break;
+    case 
+        (lang = 'en'): console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'); 
+        break;
+    default: console.log('По вашему запросу ничего не найдено');
+}
+
+function results (lang){
+   
+    let myArray = {
+        'ru': 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье',
+        'en': 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'
+    }
+
+    console.log(myArray[lang]);
+    
+}
+
+results(lang);
+
+
 
 // 2nd task
 let namePerson = prompt('Вас зовут Артем или Максим?', '');
