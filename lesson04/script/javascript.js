3924e7984adbbc393c841056357b25a77fffa860
Vasiliy Emelin <vemelin@gmail.com>
2020-03-31 16:45:12 -0500

    First updated by lesson 4

--- lesson04/script/javascript.js
+++ lesson04/script/javascript.js
@@ -1,28 +1,10 @@
 'use sctrict';
-
-let money = 1000;
-console.log(money + '$');
-
 let income = 'Freelance';
-console.log(income);
-
-let addExpenses = 'Car, Internet, Insurance, Apartments, Mobile';
-console.log(addExpenses.length);
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
-
-let deposit = true;
-console.log(deposit);
-
 let mission = 20000;
-console.log('Цель заработать ' + mission + ' долларов');
-
-let period = 24;
-console.log('Период равен ' + period + ' месяцев');
 
 //New criteria from lesson 3
 money = +prompt('Ваш месячный доход?', 5000);
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Car, Internet, Insurance, Apartments, Mobile');
 deposit = confirm('Есть ли у вас депозит в банке?');
 
 let expenses1 = prompt('Введите обязательную статью расходов?', 'Expenses 1');
@@ -31,18 +13,46 @@ let amount1 = +prompt('1 Количество: Во сколько это обо
 let expenses2 = prompt('Введите обязательную статью расходов?', 'Expenses 2');
 let amount2 = +prompt('1 Количество: Во сколько это обойдется?', 375);
 
-let budgetMonth = money - (amount1 + amount2);
-console.log(budgetMonth);
 
-let budgetDay = Math.ceil(budgetMonth/30);
-console.log(budgetDay);
+//New criteria by lesson 4
+let showTypeOff = function(data){
+    console.log(data, typeof(data));
+};
 
-if (budgetDay > 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay <= 1200) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay < 0) { 
-    console.log('Что то пошло не так');
+showTypeOff(money);
+showTypeOff(income);
+showTypeOff(deposit);
+
+let getExpensesMonth = function(){
+    return amount1 + amount2
+};
+
+let getAccumulatedMonth = function(){
+    return money - (amount1 + amount2);
+};
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Car, Internet, Insurance, Apartments, Mobile');
+
+let accumulatedMonth = getAccumulatedMonth();
+
+let getTargetMonth = function(accumulatedMonth){
+    let budgetDay = Math.ceil(accumulatedMonth/30);
+    let sum = mission / budgetDay;
+     return Math.floor(sum);
 }
+
+let budgetDay = getAccumulatedMonth();
+
+let getStatusIncome = function(){
+    if (budgetDay > 1200) {
+        return ('У вас высокий уровень дохода');
+    } else if (budgetDay >= 600 && budgetDay <= 1200) {
+        return ('У вас средний уровень дохода');
+    } else if (budgetDay < 600) {
+        return ('К сожалению у вас уровень дохода ниже среднего');
+    } else if (budgetDay < 0) { 
+        return ('Что то пошло не так');
+    }
+};
+
+console.log(getStatusIncome());
