185977948072ab96b7950242c821eab4611b15c8
Vasiliy Emelin <vemelin@gmail.com>
2020-04-01 16:33:01 -0500

    new iteration of lesson04_advanced

--- lesson04_advanced/script/javascript.js
+++ lesson04_advanced/script/javascript.js
@@ -1,7 +1,18 @@
 'use sctrict';
 
-let argResults = function(data){
-    return data + ' >> ' + typeof(data);
-};
+// let argResults = function(data){
+    
+//     if(typeof(data) != String()){
 
-console.log(argResults('Hey-yo vasko'));
+//         console.log('data');
+
+//     }
+//     // return data + ' >> ' + typeof(data);
+//     // else if(){
+
+//     // }
+// };
+
+// console.log(argResults('Hey-yo vasko'));
+
+//     //console.log(data.trim());
