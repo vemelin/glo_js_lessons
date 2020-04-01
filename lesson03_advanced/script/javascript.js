7ed6b08fb2001a06c83eeb39d5ea6278a97f199b
Vasiliy Emelin <vemelin@gmail.com>
2020-04-01 00:22:57 -0500

    Advanced lesson 03 updates

--- lesson03_advanced/script/javascript.js
+++ lesson03_advanced/script/javascript.js
@@ -3,41 +3,33 @@
 // 1st task
 let lang = prompt('Выберите язык используя короткие префиксы ru или en', '');
 
-if (lang == 'ru') {
+if (lang === 'ru') {
     console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
-} else if (lang == 'en') {
+} else if (lang === 'en') {
     console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday')
 } else {
     console.log('По вашему запросу ничего не найдено');
 }
 
 switch(lang) {
-    case (lang = 'ru'): 
+    case ('ru'): 
         console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'); 
         break;
     case 
-        (lang = 'en'): console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'); 
+        ('en'): console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'); 
         break;
     default: console.log('По вашему запросу ничего не найдено');
 }
 
-let ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-let en = 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday';
+let langArray = new Map([
+    ['ru', ['Понедельник, Вторник, Среда, Четверг, Пятница Суббота, Воскресение']],
+    ['en', ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']],
+]);
 
-let myArray = [ru, en];
-
-console.log();
-
-lang = [
-    ['ru', 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'], 
-    ['en', 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'],
-] 
-
-console.log(lang[0][1]);
-
+console.log(langArray.get(lang));
 
 // 2nd task
 let namePerson = prompt('Вас зовут Артем или Максим?', '');
-(namePerson == 'Артем') ? console.log('Директор') :  
-(namePerson == 'Максим') ? console.log('Преподаватель') :  
+(namePerson === 'Артем') ? console.log('Директор') :  
+(namePerson === 'Максим') ? console.log('Преподаватель') :  
 console.log('Студент');
