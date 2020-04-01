6ab4f2525987438e6ce2dc11c27c71c8275152e8
Vasiliy Emelin <vemelin@gmail.com>
2020-03-31 23:07:42 -0500

    lesson 3 advanced updates

--- lesson03_advanced/script/javascript.js
+++ lesson03_advanced/script/javascript.js
@@ -3,52 +3,37 @@
 // 1st task
 let lang = prompt('Выберите язык используя короткие префиксы ru или en', '');
 
-// if (lang == 'ru') {
-//     console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
-// } else if (lang == 'en') {
-//     console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday')
-// } else {
-//     console.log('По вашему запросу ничего не найдено');
-// }
-
-// switch(lang) {
-//     case (lang = 'ru'): 
-//         console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'); 
-//         break;
-//     case 
-//         (lang = 'en'): console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'); 
-//         break;
-//     default: console.log('По вашему запросу ничего не найдено');
-// }
-
-// let ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-// let en = 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday';
-
-// let myArray = [ru, en];
-
-// console.log();
-
-lang = [
-    'ru': 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье', 
-    'en': 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday' 
-] 
-
-
-function first(y){
-    console.log(0);
-    y();
+if (lang == 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+} else if (lang == 'en') {
+    console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday')
+} else {
+    console.log('По вашему запросу ничего не найдено');
 }
-function second(){
-    console.log(1);
+
+switch(lang) {
+    case (lang = 'ru'): 
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'); 
+        break;
+    case 
+        (lang = 'en'): console.log('Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'); 
+        break;
+    default: console.log('По вашему запросу ничего не найдено');
 }
-first(second);
 
 let ru = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
 let en = 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday';
 
 let myArray = [ru, en];
 
-console.log(myArray[1]);
+console.log();
+
+lang = [
+    ['ru', 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'], 
+    ['en', 'Monday, Tuesday, Wensday, Thursday, Friday, Saturday, Sunday'],
+] 
+
+console.log(lang[0][1]);
 
 
 // 2nd task
