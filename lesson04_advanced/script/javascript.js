8bddc9cc235b5a203fa43bd4ec28126deb13c2c2
Vasiliy Emelin <vemelin@gmail.com>
2020-04-02 12:38:38 -0500

    new updates

--- lesson04_advanced/script/javascript.js
+++ lesson04_advanced/script/javascript.js
@@ -4,18 +4,16 @@ let argResults = function(data){
     
     if (!isNaN(data)){
         console.log('Вы получили число');
-    }else if (typeof(data) === 'string' && data.length < 30){
-        console.log(data.trim());
-    }else if (data.length >= 30){
+    }else if (typeof(data) === 'string' && data.length >= 30){
+        data.trim();
         console.log(data.substring(0, 30) + '...');
+    }else{
+        console.log(data);
     }
 };
 
 // Push number
 // argResults(2020);
-
-// Push stroke with length < 30
-// argResults('Hey-yo vasko this is your awesome and finaly decisions by advanced lesson04');
 
 // Push stroke with length > 30
 argResults('Hey-yo vasko this is your awesome and finaly decisions by advanced lesson04');
