bc61e324b861f729bdd5f6c9ce2f103a1096248e
Vasiliy Emelin <vemelin@gmail.com>
2020-04-07 12:47:01 -0500

    2nd updates moved code from another file to the primary

--- lesson09/script/javascript.js
+++ lesson09/script/javascript.js
@@ -11,7 +11,7 @@ let money,
         } while(!isNumber(money) || money.trim() === '' || money === null);
     };
 
-    start();
+    //start();
 
 let appData = { 
     income:                     {},
@@ -160,12 +160,13 @@ let appData = {
                                 },
 };
 
-appData.asking();
-appData.getExpensesMonth();
-appData.getBudget();
-appData.getTargetMonth();
-appData.getStatusIncome();
-appData.getInfoDeposit();
+// Список обязательных методово для вызова
+// appData.asking();
+// appData.getExpensesMonth();
+// appData.getBudget();
+// appData.getTargetMonth();
+// appData.getStatusIncome();
+// appData.getInfoDeposit();
 
 //Total expenses per month (Total income - expenses)
 console.log('Расходы за месяц: ' + appData.expensesMonth);
@@ -185,4 +186,58 @@ console.log('⭕писание объекта appData:');
 
 for (let key in appData) {
     console.log(key + ': ' + appData[key]);
+    
 }
+
+
+        //Кнопку "Рассчитать" через id
+let     startBtn = document.getElementById('start'),
+
+        //Кнопки “+” (плюс) через Tag, каждую в своей переменной. 
+        addIncomePlusBtn = document.getElementsByTagName('button')[0],
+        addExpensePlusBtn = document.getElementsByTagName('button')[1],
+
+        //Чекбокс по id через querySelector
+        checkDeposit = document.querySelector('#deposit-check'),
+
+        //Поля для ввода возможных доходов (additional_income-item) при помощи querySelectorAll
+        inputsAdditionalIncome = document.querySelectorAll('.additional_income-item'),
+
+        //Каждый элемент в правой части программы через класс, которые имеют в имени класса "-value"
+        outputExpensesResults = document.getElementsByClassName('result-total'),
+
+        expensesResults = function () {
+
+            for (let i = 1; i < outputExpensesResults.length; i++) {
+                let result;
+                result = document.getElementsByClassName('result-total')[i];
+                console.log(result);
+            }
+        };
+        expensesResults();
+
+
+        // Оставшиеся поля через querySelector каждый в отдельную переменную:
+        // Месячный доход *
+let     salaryAmountInput = document.querySelector('.salary-amount'),
+
+        // Дополнительный доход
+        incomeNameInput = document.querySelector('.income-title'),
+        incomeAmountInput = document.querySelector('.income-amount'),
+
+        // Возможный доход
+        additionalIncomeNameInput1 = document.querySelector('.additional_income-item'),
+        additionalIncomeNameInput2 = document.querySelector('.additional_income-item'),
+
+        // Возможный доход
+        requiredExepnsesNameInput = document.querySelector('.expenses-title'),
+        requiredExepnsesAmountInput = document.querySelector('.expenses-amount'),
+
+        // Возможные расходы (перечислите через запятую)
+        additionalExpensesListInput = document.querySelector('.additional_expenses-item'),
+
+        // Цель
+        targetAmountInput = document.querySelector('.target-amount'),
+
+        // Период расчета
+        periodInput = document.querySelector('.period-select');
