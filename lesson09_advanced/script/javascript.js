4d8ebe7cc9ba6119d61d6bd7781057fb25798760
Vasiliy Emelin <vemelin@gmail.com>
2020-04-07 19:29:07 -0500

    added a part of code from previous lesson

--- lesson09_advanced/script/javascript.js
+++ lesson09_advanced/script/javascript.js
@@ -67,3 +67,35 @@ document.write(fullDate);
 // console.log(dateFormat);
 
 // document.write (date);
+
+
+
+
+
+// let weekDaysArray = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+// let months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", 
+//             "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+// let myDate = new Date();
+
+// for (let i = 0; i < weekDaysArray.length; i++) {
+  
+    
+//     let currentDay = new Date().getDay() - 1;
+
+//     if (weekDaysArray[i] == 'Суббота' || weekDaysArray[i] == 'Воскресенье') {
+
+//             document.write(`<p><i>${weekDaysArray[i]}</i></p>`);
+
+//     } else if (i === myDate.getDay()) {
+
+//             document.write(`<p> <b>  ${weekDaysArray[myDate.getDay()]} </b> - <i> (Для справки сегодня ${myDate.getDate() + ' ' + months[myDate.getMonth()]} )</i></p>`);
+
+//     } else {
+
+//             document.write(`<p> ${weekDaysArray[i]} </p>`);
+
+//     }
+
+// }
