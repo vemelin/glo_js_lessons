86adf7ca4678e086fe079e3757e08fcf7c67d50d
Vasiliy Emelin <vemelin@gmail.com>
2020-04-14 22:47:58 -0500

    second updates by first part of 14 lesson

--- lesson14-1/script/javascript.js
+++ lesson14-1/script/javascript.js
@@ -1,38 +1,37 @@
 'use strict';
 const body = document.querySelector('body');
 
-function MainProps(selector, height, width, bg, fontSize) {
-	this.selector = selector;
-	this.height = height;
-	this.width = width;
-	this.bg = bg;
-	this.fontSize = fontSize;
+class MainProps {
+	constructor(selector, height, width, bg, fontSize) {
+		this.selector = selector;
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+	}
+	renderElements() {
+		let elems;
+		if (this.selector[0] === '.') {
+			elems = document.createElement('div');
+			elems.classList.add('block');
+		} else if (this.selector[0] === '#') {
+			elems = document.createElement('p');
+			elems.classList.add('nameById');
+		}
+		elems.style.cssText = `
+    height: ${this.height}px;
+    width: ${this.width}px;
+    background: ${this.bg};
+    font-size: ${this.fontSize}px;
+  `;
+		elems.textContent = 'Param-pam-pam';
+		body.append(elems);
+		console.log('Element ⥤', elems);
+	}
 }
 
-MainProps.prototype.renderElements = function() {
-	let elems;
-	if (this.selector[0] === '.') {
-		elems = document.createElement('div');
-		elems.classList.add('block');
-	} else if (this.selector[0] === '#') {
-		elems = document.createElement('p');
-		elems.classList.add('nameById');
-	}
-	elems.style.cssText = `
-    height: ${this.height} px;
-    width: ${this.width} px;
-    background: ${this.bg};
-    font-size: ${this.fontSize} px;
-  `;
-	elems.textContent = 'Param-pam-pam';
-	body.append(elems);
-	console.log('Element: ', elems);
-};
-
-//Link to new Object
-// const linkToObject = new domElement.mainProps('new_object', 70, 70, '#030303', 35);
-const results1 = new MainProps('.id', 70, 70, 'red', 35);
+const results1 = new MainProps('.id', 70, 70, 'red', 18);
 results1.renderElements();
 
-const results2 = new MainProps('#id', 50, 50, 'yellow', 10);
+const results2 = new MainProps('#id', 50, 50, 'yellow', 20);
 results2.renderElements();
