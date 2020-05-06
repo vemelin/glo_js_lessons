c55df01c613a8de319744ad9565b3243a569c8c8
Vasiliy Emelin <vemelin@gmail.com>
2020-05-06 02:16:03 -0500

    1st iteration of work by lesson26_advanced type

---
+++ lesson26_advanced/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    select.addEventListener('change', () => {
+
+      return new Promise((resolve, reject) => {
+
+        const request = new XMLHttpRequest();
+        request.open('GET', './cars.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                    }
+                });
+            resolve();
+            } else {
+                output.innerHTML = 'Произошла ошибка';
+                reject(request.status);
+            }
+        });
+
+      });
+
+    });
+
+});
