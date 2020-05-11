975bc6b511c652d2fc5dce39aa2ca498217047a3
Vasiliy Emelin <vemelin@gmail.com>
2020-05-10 21:48:50 -0500

    ready to productions, last updates by 28 lessond

--- lesson28/script/index.js
+++ lesson28/script/index.js
@@ -1,3 +1,21 @@
+// Polyfills
+import 'url-polyfill';
+import "regenerator-runtime/runtime";
+import 'element-remove';
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import 'formdata-polyfill';
+import elementClosest from 'element-closest';
+import 'es6-promise';
+import 'element-closest/browser';
+import 'whatwg-fetch';
+import "scroll-behavior-polyfill";
+import smoothscroll from 'smoothscroll-polyfill';
+import 'element-remove';
+elementClosest(window);
+smoothscroll.polyfill();
+
+// Import list of Modules
 import countTimer from './modules/countTimer';
 import togglleMenu from './modules/toggleMenu';
 import smoothScroll from './modules/smoothScroll';
@@ -8,41 +26,21 @@ import ourTeamPic from './modules/ourTeamBlock';
 import calculatorBlock from './modules/calculatorBlock';
 import sendForm from './modules/ajaxRequest';
 
-// Polyfills 
-import '@babel/polyfill';
-import 'nodelist-foreach-polyfill';
-import elementClosest from 'element-closest';
-import 'formdata-polyfill';
-import 'fetch-polyfill';
-import smoothscroll from 'smoothscroll-polyfill';
-import 'element-remove';
-
-elementClosest(window);
-smoothscroll.polyfill();
-
 // countTimer('Put your time');
 countTimer("21 april 2020");
-
 // Main navigation
 togglleMenu();
-
 // Main slow mo
 smoothScroll();
-
 // Modal window
 modalToggle();
-
 // Our Services block
 serviceBlock();
-
 // Portfolio Image Slider Scroll
 slider();
-
 // Our team block
 ourTeamPic();
-
 // Interior calculator block
 calculatorBlock(100);
-
 // Send Form through AJAX function
 sendForm();
